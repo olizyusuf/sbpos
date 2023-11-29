92de917fdb9b6b2b2adbfe25ecab5da731885d78
olizyusuf <olizyusuf22@gmail.com>
2023-11-29 22:37:57 +0700

    penjualan: design penjualan

--- lib/ui/penjualan_screen.dart
+++ lib/ui/penjualan_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sbpos/ui/utils.dart';
 
 class PenjualanScreen extends StatelessWidget {
   const PenjualanScreen({super.key});
@@ -10,9 +11,38 @@ class PenjualanScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: Text("halaman penjualan"),
-      ),
+      body: SizedBox(
+          width: displayWidth(context),
+          height: displayHeight(context),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 5, right: 5),
+                child: TextField(),
+              ),
+              Container(
+                  height: displayHeight(context) * 0.6,
+                  child: ListView.builder(
+                    itemCount: 20,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text("test"),
+                      );
+                    },
+                  )),
+              Expanded(
+                child: Row(
+                  children: [
+                    Container(
+                      color: Colors.blue,
+                      width: 275,
+                    ),
+                    Expanded(child: Text("Total"))
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
